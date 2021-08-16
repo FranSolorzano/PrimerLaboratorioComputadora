af4e4160d4f0d55bed1bd33a9bdf8aa8c515cb12
FranSolorzano <87993086+FranSolorzano@users.noreply.github.com>
2021-08-16 14:23:21 -0600

    Actualizacion2.FS

--- Computadora/Computadora/Entidades/ClsComputadora.cs
+++ Computadora/Computadora/Entidades/ClsComputadora.cs
@@ -8,5 +8,26 @@ namespace Computadora.Entidades
 {
     class ClsComputadora
     {
+        private String memoriaRam;
+        private String tarjetaMadre;
+        private String procesador;
+        private String discoDuro;
+        private String teclado;
+        private String mouse;
+        private String monitor;
+        private String tarjetaGrafica;
+        private String entradaUsb;
+        private String bateria;
+
+        public string MemoriaRam { get => memoriaRam; set => memoriaRam = value; }
+        public string TarjetaMadre { get => tarjetaMadre; set => tarjetaMadre = value; }
+        public string Procesador { get => procesador; set => procesador = value; }
+        public string DiscoDuro { get => discoDuro; set => discoDuro = value; }
+        public string Teclado { get => teclado; set => teclado = value; }
+        public string Mouse { get => mouse; set => mouse = value; }
+        public string Monitor { get => monitor; set => monitor = value; }
+        public string TarjetaGrafica { get => tarjetaGrafica; set => tarjetaGrafica = value; }
+        public string EntradaUsb { get => entradaUsb; set => entradaUsb = value; }
+        public string Bateria { get => bateria; set => bateria = value; }
     }
 }
